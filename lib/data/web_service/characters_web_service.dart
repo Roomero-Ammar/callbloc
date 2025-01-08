7ac1bb85739876ffb8f496fe9b5a4a534806cbe7
AmmarYaser <ay1513600@gmail.com>
2025-01-08 15:46:17 +0200

    create web_service Initialize Dio and implement getAllCharacters method
    Added Dio initialization with BaseOptions in the CharactersWebService constructor. Implemented the getAllCharacters method to fetch characters data from the web service and handle potential errors.

--- lib/data/web_service/characters_web_service.dart
+++ lib/data/web_service/characters_web_service.dart
@@ -1,3 +1,29 @@
+import 'package:dio/dio.dart';
+
+import '../../constants/app_strings.dart';
+
 class CharactersWebService {
-  Dio dio;
+ late Dio dio;
+
+ CharactersWebService() {
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl,
+      receiveDataWhenStatusError: true,
+      connectTimeout: const Duration(seconds: 100), // Set connect timeout
+      receiveTimeout: const Duration(seconds: 100), // Set receive timeout
+    );
+    dio = Dio(options);
+  }
+
+
+  Future<List<dynamic>> getAllCharacters() async {
+    try {
+      Response response = await dio.get('characters');
+      print(response.data.toString());
+      return response.data;
+    } catch (e) {
+      print('Error fetching characters: $e');
+      return []; // Return an empty list in case of an error
+    }
+  }
 }

3df7799a02cde31843b642324deb29fc259b4885
AmmarYaser <ay1513600@gmail.com>
2025-01-08 15:47:14 +0200

    Add CharactersCubit and CharactersState for managing character data
    Implemented CharactersCubit to handle character data using the BLoC pattern. Added CharactersState to define the different states of character data loading.

---
+++ lib/manager/cubit/characters_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:callbloc/data/models/characters/characters.dart';
+import 'package:callbloc/data/repo/characters_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'characters_state.dart';
+
+class CharactersCubit extends Cubit<CharactersState> {
+final CharactersRepo charactersRepo;
+late List<Character> characters; //
+
+  CharactersCubit(this.charactersRepo) : super(CharactersInitial());
+
+  List <Character> getAllCharacters(){
+    charactersRepo.getAllCharacters().then((characters){  //-
+      emit(CharactersLoaded(characters));  //-
+      this.characters;  //
+    });
+    return characters;  //
+  }
+}

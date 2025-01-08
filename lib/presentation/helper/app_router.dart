05cca9d9cd649cbbee7420293e1d51832c461d71
AmmarYaser <ay1513600@gmail.com>
2025-01-08 20:05:02 +0200

    Initialize CharactersCubit and provide it to CharactersScreen
    Added initialization of `CharactersRepo` and `CharactersCubit` in `AppRouter`. Wrapped `CharactersScreen` with `BlocProvider` to provide the `CharactersCubit` instance.

--- lib/presentation/helper/app_router.dart
+++ lib/presentation/helper/app_router.dart
@@ -1,19 +1,34 @@
 import 'package:callbloc/constants/app_strings.dart';
+import 'package:callbloc/data/repo/characters_repo.dart';
+import 'package:callbloc/data/web_service/characters_web_service.dart';
+import 'package:callbloc/manager/cubit/characters_cubit.dart';
 import 'package:callbloc/presentation/screens/character_details_screen.dart';
 import 'package:callbloc/presentation/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class AppRouter {
-  Route? generateRoute(RouteSettings settings){
-    switch (settings.name){
+  late CharactersRepo charactersRepo;
+  late CharactersCubit charactersCubit;
 
+  AppRouter() {
+    charactersRepo = CharactersRepo(CharactersWebService());
+    charactersCubit = CharactersCubit(charactersRepo);
+  }
 
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
       case charactersScreen:
-      return MaterialPageRoute(builder: (_)=> CharactersScreen());
-    
-      case characterDetailsScreen:
-      return MaterialPageRoute(builder: (_)=> CharacterDetailsScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (BuildContext context) =>
+                      charactersCubit,
+                  child: CharactersScreen(),
+                ));
 
+      case characterDetailsScreen:
+        return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
     }
   }
 }
